18f1ae73a874652c5807182aa02e2973a3efe14c
Jun Clemente <kjstudios858@gmail.com>
2016-01-28 00:26:30 -0800

    working on fixing Enemy speed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, level) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // Enemy coordinates
     this.x = x;
     this.y = y;
-    this.level = 1;
-    this.speed = ((Math.random() * 10) + 50);
+    this.speed = ((Math.random() * 10) + 100);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -19,14 +18,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    var count = 0;
     if (this.x < 505) {
-        this.x = this.x + (this.level * (this.speed * dt));
+        this.x = this.x + (this.speed * dt);
     } else {
-        // this.speed = ((Math.random() * 100) + 1);
-        this.speed += 20;
-        this.x = -90 + (this.level * (this.speed * dt));
-
+        this.speed = ((Math.random() * 10) + 100);
+        this.x = -90 + (this.speed * dt);
     }
     // Reverse movement
     // if (this.x > -90) {
@@ -72,7 +68,7 @@ var Player = function(x, y) {
             this.resetPosition();
             this.score += 10;
         }
-    }
+    };
 
     this.collisionCheck = function() {
         var spriteHeight = 83,
@@ -83,25 +79,26 @@ var Player = function(x, y) {
                 spriteRight = spriteLeft + 158;
 
             if (playerLoc[0] > spriteLeft && playerLoc[0] < spriteRight) {
-                if (playerLoc[1] > allEnemies[i].y && playerLoc[1] < allEnemies[i].y + spriteHeight) {
+                if (playerLoc[1] > allEnemies[i].y &&
+                    playerLoc[1] < allEnemies[i].y + spriteHeight) {
                     return true;
                 }
             }
         }
         return false;
-    }
+    };
 
     this.homeCheck = function() {
         if (this.y < 71) {
             return true;
         }
         return false;
-    }
+    };
 
     this.resetPosition = function(status) {
         this.x = 202;
         this.y = 403;
-    }
+    };
 
     this.render = function() {
         var scoreLoc = [15, 570],
@@ -124,7 +121,7 @@ var Player = function(x, y) {
         ctx.strokeText(this.message, msgLoc[0], msgLoc[1]);
         // Draw Character
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+    };
 
     // For each acceptable keyPress, this function will update the x, y
     // coordinates of the Player class.
@@ -133,36 +130,32 @@ var Player = function(x, y) {
             yMove = 83;
 
         if (keyPress == 'spacebar') {
-            this.demoMode == true;
-            this.message = "";
+            this.demoMode = false;
+            this.message = "Let's go!";
         }
-        if (this.demoMode == false) {
+        if (this.demoMode === true) {
             this.message = "Press Space Bar to Play.";
         }
-        if (this.demoMode == true) {
+        if (this.demoMode === false) {
             switch (keyPress) {
-
                 case 'left':
                     // console.log('pressed left');
                     if (this.x > 0) {
                         this.x -= xMove;
                     }
                     break;
-
                 case 'right':
                     // console.log('pressed right');
                     if (this.x < ctx.canvas.clientWidth - xMove) {
                         this.x += xMove;
                     }
                     break;
-
                 case 'down':
                     // console.log('pressed down');
                     if (this.y < ctx.canvas.clientHeight - (yMove * 3)) {
                         this.y += yMove;
                     }
                     break;
-
                 case 'up':
                     // console.log('pressed up');
                     if (this.y > 0) {
@@ -171,25 +164,39 @@ var Player = function(x, y) {
                     break;
             }
         }
-    }
-}
+    };
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+// var allEnemies = []
+
+// var createEnemy = function(level) {
+//     var enemyRow = [60, 143, 227];
+//     var i = 0;
+//     while (i < 4) {
+//         var randomRow = enemyRow[Math.floor(Math.random() * enemyRow.length)];
+//         console.log(randomRow);
+//         allEnemies[i] = new Enemy(-70, randomRow);
+//         i++;
+//     }
+// };
+// createEnemy();
+
+
 var foeTop = new Enemy(-70, 60),
     foeMid = new Enemy(-70, 143),
     foeBot = new Enemy(-70, 227);
 var allEnemies = [foeTop, foeMid, foeBot];
-// var allEnemies = [foeTop];
 var player = new Player(202, 403);
-
+var level = 1;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     var allowedKeys = {
         // Arrow keys
         37: 'left',
